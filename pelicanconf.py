fee34dee58f0aafe01c66df7f167883e328fdca2
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-07 21:50:32 +0100

    Added some social links and removed other links

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,14 +18,15 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
+"""LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
+"""
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/raulcumplido'),
+          ('github', 'http://github.com/raulcd'),)
 
 
 STATIC_PATHS = ['extra/CNAME']
